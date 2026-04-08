99b2acd4f3f46a6364e73843dac1fb8ddcfb23a8
Satyam Kumar <satyamx10@gmail.com>
2026-04-08 11:12:51 +0530

    fix

--- src/components/WallCalendar/WallCalendar.tsx
+++ src/components/WallCalendar/WallCalendar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './WallCalendar.module.css';
 import HeroSection from './HeroSection';
 import NotesSection from './NotesSection';
